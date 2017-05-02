d60a97fcf4839ac12d9b1f219dbe579c94eb9c4b
Sriram Sreedhar <sriram003@gmail.com>
2017-05-02 15:00:46 -0700

    Update mysql_connect.py

--- mysql_connect.py
+++ mysql_connect.py
@@ -7,3 +7,7 @@ for r in cur:
     print(r)
 cur.close()
 conn.close()
+
+#if you see ConnectionRefusedError: [Errno 111] Connection refused, follow the below steps
+#[root@sriram mysql]# cd /tmp/
+#[root@sriram tmp]# ln -s /var/lib/mysql/mysql.sock mysql.sock
